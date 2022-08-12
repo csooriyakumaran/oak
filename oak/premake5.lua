637139faa41f087a2c44ee82b33592a903b7ca8b
c. sooriyakumaran <c.sooriyakumaran@gmail.com>
2022-08-12 11:39:16 -0400

    added glm submodule

--- oak/premake5.lua
+++ oak/premake5.lua
@@ -25,31 +25,34 @@ project "Oak"
     }
     links
     {
-        "GLFW",
-        "gdi32",
-        "Dwmapi",
-        "ImPlot",
-        "C:/VulkanSDK/1.3.216.0/Lib/vulkan-1",
+        -- "GLFW",
+        -- "gdi32",
+        -- "Dwmapi",
+        -- "ImPlot",
+        -- "C:/VulkanSDK/1.3.216.0/Lib/vulkan-1",
     }
     targetdir ("bin/" .. outputdir .. "/%{prj.name}")
     objdir ("build/" .. outputdir .. "/%{prj.name}")
 
     postbuildcommands{
-        "{MOVE} %{cfg.buildtarget.abspath} temp.a",
        
+        "{MKDIR} build/" ..outputdir .."/oak-obj",
+        "ar -xv  %{cfg.buildtarget.abspath} --output build/" .. outputdir .. "/oak-obj",
+              
         "{MKDIR} build/" ..outputdir .."/ImPlot-obj",
         "{CHDIR} .. && ar -xv vendor/implot/bin/" .. outputdir .. "/ImPlot/ImPlot.lib --output oak/build/" .. outputdir .. "/ImPlot-obj",
        
         "{MKDIR} build/" ..outputdir .."/GLFW-obj",
         "CHDIR .. && ar -xv vendor/glfw/bin/" .. outputdir .. "/GLFW/GLFW.lib --output oak/build/" .. outputdir .. "/GLFW-obj",
        
-        "ar -rcs %{cfg.buildtarget.abspath} build/" .. outputdir .."/ImPlot-obj/*.o build/" .. outputdir .."/GLFW-obj/*.o build/" .. outputdir .."/Oak/*.o",
+        "ar -rcs %{cfg.buildtarget.abspath} build/" .. outputdir .."/ImPlot-obj/*.o build/" .. outputdir .."/GLFW-obj/*.o build/" .. outputdir .."/oak-obj/*.o",
 
         -- "{MKDIR} build/" ..outputdir .."/vulkan-obj",
         -- "ar -xv C:/VulkanSDK/1.3.216.0/Lib/vulkan-1.lib --output build/" .. outputdir .. "/vulkan-obj",
 
         "{RMDIR} build/" ..outputdir .."/ImPlot-obj",
         "{RMDIR} build/" ..outputdir .."/GLFW-obj",
+        "{RMDIR} build/" ..outputdir .."/oak-obj",
     }
 
     filter "system:windows"
